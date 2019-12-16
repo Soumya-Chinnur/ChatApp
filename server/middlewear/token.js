877c8d3c5761d8cb44f81374055fc12f51836caf
PrafulGupta <soumyachinnur@gmail.com>
2019-12-16 19:26:04 +0530

    [soumya] Refactor.modified token for better readiability and pushed into remote

--- server/middlewear/token.js
+++ server/middlewear/token.js
@@ -11,7 +11,7 @@ exports.GenerateToken = (payload) => {
     }
 }
 exports.verify = (request, res, next) => {
-    console.log("Verifies request");
+    console.log("Verifies request");//verifies request 
     var token = request.params.token;
     jwt.verify(token, process.env.KEY, (err, result) => {
         if (err) res.status(422).send({ message: "token is not correct" });
