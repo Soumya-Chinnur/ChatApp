b5f0bb25b00a7450de39a2efcbb16bffd4f6b5f0
Soumya-Chinnur <soumyachinnur@gmail.com>
2020-01-11 09:35:30 +0530

    [Soumya] refactor.modified server  program and pushed into remote

--- server/server.js
+++ server/server.js
@@ -6,37 +6,62 @@
 const express = require('express')//allows to setup middlewares
 const bodyParser = require('body-parser');//body parsing middleware
 const app = express();//creating an express app
-const routes = require('./router/router.js')
+const routes = require('./router/router')
 //it is a middleware that validates the body of a request and returns a response with error if the validation rules fail
 const expressValidator = require('express-validator')
-const dbconfig = require('./configuration/dbConfig')
+const dbconfig = require('./configuration/dbconfig')
 const mongoose = require('mongoose');
+var cors = require('cors')
+var userControl = require('./controller/userController')
+
 
 require('dotenv').config()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))//urlencoded is used to translate unprintable characters
 app.use(expressValidator())
-
+app.use(cors())
 app.use('/', routes);//router is attached to an instance of the express class.
 
-//connecting to the database
 console.log(dbconfig.url);
 
 mongoose.connect(dbconfig.url, {
-    //to pass the new parser and connect to the mongoose.connect
     useNewUrlParser: true, useUnifiedTopology: true
 })
     //.then() is used to return a promise
     .then(() => {
         console.log("Successfully connected to the database");
     })
-    .catch(() => { 
+    .catch(() => {
         console.log('Could not connect to the database');
         process.exit();
     })
 //loads environment variables from dotenv files
-app.listen(process.env.PORT, () => {
-    console.log("Server is listening on port 3000")
+// app.listen(process.env.PORT, () => {
+//     console.log("Server is listening on port 3000")
+// });
+var server = app.listen(process.env.PORT, () => {
+    console.log("Server is listing on port 3000")
 });
- 
+const io = require('socket.io').listen(server);
+io.on('connection', (socket) => {
+    console.log("user connected")
+    socket.on('newMsg', data => {
+        console.log("data in sockets", data);
+
+        console.log("chat data", userController.sendMessage(data))
+        io.sockets.emit('Message', data)
+    })
+})
+var server = app.listen(3000, () => {
+    console.log("Server is listing on port 3001");
+    });
+    const io = require('socket.io').listen(server);
+    io.on('connection', (socket) => {
+    console.log('user connected now...')
+    socket.on('newMsg', data => {
+    console.log('data of socket', data);
+    controller.sendMessage(data);
+    
+    })
+module.exports = {app}
